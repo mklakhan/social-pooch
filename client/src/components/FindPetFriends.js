83daa35d12d7657438d3d6f2f6170c5d6756d9b7
mklakhan <mklakhan@gmail.com>
2021-04-10 10:10:39 -0700

    updates to findpetfriends.js in components

--- client/src/components/FindPetFriends.js
+++ client/src/components/FindPetFriends.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {Button, Container, Row, Col, Form, Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 
 export default function FindPetFriends() {
     return (
-        <div>
-        <h1>I'm in the Find Pet Friends page</h1> 
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://freepngimg.com/thumb/dog/1-2-dog-png-10-thumb.png" />
+            <Card.Body>
+                <Card.Title>Rufus</Card.Title>
+                <Card.Text>
+                    I enjoy playdates at the park with my friends!
+    </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Chicago, IL</ListGroupItem>
+                <ListGroupItem>Male</ListGroupItem>
+                <ListGroupItem>2 Years Old</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">Dislike</Card.Link>
+                <Card.Link href="#">Like</Card.Link>
+            </Card.Body>
+        </Card>
     )
 }
+
+
+
