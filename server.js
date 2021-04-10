57228cdaf723db30ebc4f2e45468a62442fcb0cd
kihBootCamp <kimberleyheuer@hotmail.com>
2021-04-10 07:04:20 -0500

    init branch

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+const mongoose = require("mongoose");
+const apiRoutes = require("./routes/api-routes.js");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost//socialpooch");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use(apiRoutes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
