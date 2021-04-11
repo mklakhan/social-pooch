dc37d27782c369cb159741e67d4f0838137bffd9
kihBootCamp <kimberleyheuer@hotmail.com>
2021-04-11 13:32:42 -0500

    start api-routing

---
+++ client/src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export default {
+  getPet: function() {
+    return axios('/api/pet')
+  },
+  getPets: function() {
+    return axios('/api/pets')
+  },
+  savePet: function(data) {
+    return axios.post('/api/pet', data)
+  }
+}
