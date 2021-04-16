fd0df83bfb5b59f7427d1f9a0d13086f75e1a22d
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-04-15 21:04:22 -0500

    got card id

--- client/src/components/PetCard.js
+++ client/src/components/PetCard.js
@@ -1,28 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Container, Row, Col, Form, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  Form,
+  Card,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 
-function PetCard( props) {
-    console.log(props)
-return (
-
-    <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={props.petPic} />
-        <Card.Body>
-            <Card.Title>{props.pet_name}</Card.Title>
-            <Card.Text>{props.playdate}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-            <ListGroupItem>{props.zipcode}</ListGroupItem>
-            <ListGroupItem>{props.gender}</ListGroupItem>
-            <ListGroupItem>{props.birthday}</ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-            <Card.Link href="#">Dislike</Card.Link>
-            <Card.Link href="#">Like</Card.Link>
-        </Card.Body>
-        
-    </Card>
-)
+function handleClick(id) {
+  console.log(id);
 }
 
-export default PetCard
+function PetCard(props) {
+  console.log(props);
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.petPic} />
+      <Card.Body>
+        <Card.Title onClick={handleClick}>{props.pet_name}</Card.Title>
+        <Card.Text>{props.playdate}</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>{props.zipcode}</ListGroupItem>
+        <ListGroupItem>{props.gender}</ListGroupItem>
+        <ListGroupItem>{props.birthday}</ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link>Dislike</Card.Link>
+        <Card.Link
+          onClick={() => {
+            handleClick(props._id);
+          }}
+          href="#"
+        >
+          Like
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PetCard;
