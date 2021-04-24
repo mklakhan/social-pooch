4035efdd0c283add0b5427234a88610fa5f4bbd3
Aika Darling <zh.aikerim@gmail.com>
2021-04-24 14:47:10 -0500

    changing comming soon file

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -8,7 +8,7 @@ export default function Banner() {
         <div>      
             <Container fluid style={{ backgroundImage: `url(${socialpoochimg})`, height: "100%", width: "100%"}}>
                 <Image style={{height: "10%", width: "100%"}} src={socialpoochimg} fluid />
-                {/* <div style={{minHeight: "75%", minWidth: "75%"}}></div> */}
+                {/* <div style={{minHeight: "75%", minWidth: "75%zxcvbghjkhgf"}}></div> */}
             </Container>
         </div>
     )
